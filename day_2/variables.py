2203a5ab6ecdc63f9801356e0e5c46b8c6e17da3
mta630 <Michael.t.albert89@gmail.com>
2020-07-09 12:41:04 -0500

    Completed Day 2!

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,4 +1,5 @@
 # Day 2: 30 Days of python programming
+import math
 
 firstName = 'Michael'
 lastName = 'Albert'
@@ -25,5 +26,21 @@ print('First name: ', len(firstName), ' Last Name: ', len(lastName));
 num_one = 5
 num_two = 4
 _total = num_one + num_two
+_diff = num_one - num_two
+_product = num_two * num_one
+_division = num_one / num_two
+_remainder = num_one % num_two
+_exp = num_one ** num_two
+_floor_division = num_one // num_two
 
 print(num_one, ' + ', num_two, ' = ', _total)
+
+area_of_circle = math.pi * 30 ** 2
+circ_of_circle = 2 * math.pi * 30
+
+print('area of circle with radius 30: ', area_of_circle)
+
+firstName = input('Enter first name: ')
+lastName = input('Enter Last Name: ')
+
+print('Full name:', firstName, lastName)
