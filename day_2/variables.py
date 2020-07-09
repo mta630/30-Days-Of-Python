0bf997d1fcdc6638b834a0ceec2bb836e9fc827b
mta630 <Michael.t.albert89@gmail.com>
2020-07-09 12:31:58 -0500

    Worked through 17 exercises

--- day_2/variables.py
+++ day_2/variables.py
@@ -2,7 +2,7 @@
 
 firstName = 'Michael'
 lastName = 'Albert'
-fullName = firstName + lastName
+fullName = firstName + ' ' + lastName
 country = 'United States'
 city = 'Normal'
 age = 31
@@ -10,7 +10,20 @@ year = 2020
 is_married = False
 is_true = True
 is_light_on = False
-fish, raven, lily = 'Pets'
+fish, dog, cat = 'baal', 'Raven', 'Lily'
 
 print(type(firstName))
 print(type(lastName))
+print(type(country))
+print(type(age))
+print(type(fish))
+print('Length of first Name: ', len(firstName))
+print(fullName)
+print('First name: ', len(firstName), ' Last Name: ', len(lastName));
+
+
+num_one = 5
+num_two = 4
+_total = num_one + num_two
+
+print(num_one, ' + ', num_two, ' = ', _total)
