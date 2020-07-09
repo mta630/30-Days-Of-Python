efdae1c272285191e71bccce5067f985c1df35e7
mta630 <Michael.t.albert89@gmail.com>
2020-07-09 12:27:16 -0500

    Working on day 2

---
+++ day_2/variables.py
@@ -0,0 +1,16 @@
+# Day 2: 30 Days of python programming
+
+firstName = 'Michael'
+lastName = 'Albert'
+fullName = firstName + lastName
+country = 'United States'
+city = 'Normal'
+age = 31
+year = 2020
+is_married = False
+is_true = True
+is_light_on = False
+fish, raven, lily = 'Pets'
+
+print(type(firstName))
+print(type(lastName))
